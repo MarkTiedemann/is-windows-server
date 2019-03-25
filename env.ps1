7bda9ecfd022f0b2d337780d648a1388c6559cb6
Mark Tiedemann <www.marktiedemann@gmail.com>
2019-03-25 22:15:28 +0100

    Print powershell version in CI

--- env.ps1
+++ env.ps1
@@ -1,3 +1,4 @@
-Write-Host BuildLabEx: (Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion').BuildLabEx
-Write-Host InstallationType: (Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion').InstallationType
+Write-Host BuildLabEx: (gp 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion').BuildLabEx
+Write-Host InstallationType: (gp 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion').InstallationType
 Write-Host ProductType: (Get-WMIObject Win32_OperatingSystem ProductType).ProductType
+Write-Host PSVersion: $PSVersionTable.PSVersion
